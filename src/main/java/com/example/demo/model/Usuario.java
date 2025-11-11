9dfc77188d3bdaa090855b595217dd7b817e2502
JaimeNeira <jaimelaika12@gmail.com>
2025-11-11 22:38:56 +0100

    1

---
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nombre;
+}
